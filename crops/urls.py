d4b1e1c4e29618db7efec52cee6039382070bbed
Bluzora-glitch <bluzorasystem@gmail.com>
2025-02-26 07:47:48 +0700

    ลบไฟล์ทั้งหมดออกจาก Git แต่ยังคงอยู่ในเครื่อง

--- crops/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import update_crop_prices_from_request, update_all_crop_prices
-
-urlpatterns = [
-    path('update-prices/', update_crop_prices_from_request, name='update-prices'),
-    path('update-all-prices/', update_all_crop_prices, name='update_all_crop_prices'),
-]
