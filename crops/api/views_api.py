2f928314324c2ef354170b29bb465d3aba1c9db1
Bluzora-glitch <bluzorasystem@gmail.com>
2025-02-28 06:33:00 +0700

    แก้ปัญหาเรียกapiโหลดช้า

--- crops/api/views_api.py
+++ crops/api/views_api.py
@@ -1,6 +1,12 @@
 from rest_framework import viewsets
+from .filters import CropVariableFilter, PredictedDataFilter
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
 from crops.models import Crop, CropVariable, PredictedData
 from .serializers import CropSerializer, CropVariableSerializer, PredictedDataSerializer
+
+class CropVariablePagination(PageNumberPagination):
+    page_size = 100  # หรือกำหนดตามที่ต้องการ
 
 class CropViewSet(viewsets.ModelViewSet):
     queryset = Crop.objects.all()
@@ -10,9 +16,15 @@ class CropViewSet(viewsets.ModelViewSet):
 class CropVariableViewSet(viewsets.ModelViewSet):
     queryset = CropVariable.objects.all()
     serializer_class = CropVariableSerializer
+    pagination_class = CropVariablePagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = CropVariableFilter
     http_method_names = ['get']  # กำหนดให้รองรับเฉพาะ GET เท่านั้น
 
 class PredictedDataViewSet(viewsets.ModelViewSet):
     queryset = PredictedData.objects.all()
     serializer_class = PredictedDataSerializer
+    pagination_class = CropVariablePagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = PredictedDataFilter
     http_method_names = ['get']  # กำหนดให้รองรับเฉพาะ GET เท่านั้น
--- price_prediction/settings.py
+++ price_prediction/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'rest_framework',
+    'django_filters',
     'crops',
 ]
 
@@ -51,6 +52,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,  # แสดง 100 rows ต่อหน้า (ปรับได้ตามความเหมาะสม)
+}
+
 
 ROOT_URLCONF = 'price_prediction.urls'
 
